48e99c59259a5139cbe150647d36e5028f367e22
shun5523 <z31sabyss@yahoo.co.jp>
2019-11-10 12:47:46 +0000

    created problem's views

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,22 +1,50 @@
 class ProblemsController < ApplicationController
 	def index
+		@problems = Problem.all
 	end
 
 	def new
+		@problem = Problem.new
 	end
 
 	def create
+		@problem = Problem.new(problem_params)
+		@problem.user_id = current_user.id
+
+		if @problem.save
+			redirect_to problems_path
+	    else
+	    	render :new
+		end
+
 	end
 
 	def show
+		@problem = Problem.find(params[:id])
 	end
 
 	def edit
+		@problem = Problem.find(params[:id])
+
 	end
 
 	def update
+		 @problem = Problem.find(params[:id])
+         @problem.user_id = current_user.id
+      if @problem.update(problem_params)
+         # flash[:notice] = "the Book was successfully edited"
+         redirect_to problem_path(@problem)
+      else
+         render :edit
+      end
+
 	end
 
 	def destroy
+	end
+
+  private
+	def problem_params
+		params.require(:problem).permit(:user_id, :title, :body, :genre)
 	end
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,6 +1,6 @@
 class User::UsersController < ApplicationController
 	def show
-		@user = User.where(id: params[:id])
+		@user = User.find(params[:id])
 	end
 
 	def edit
@@ -8,9 +8,21 @@ class User::UsersController < ApplicationController
 	end
 
 	def update
+		user = User.new(user_params)
+		user.save
+		visit = Visit.new(visit_params)
+		visit.user_id = user.user_id
 	end
 
 	def destroy
 	end
 
+  private
+	def user_params
+		params.require(:user).permit(:user_name, :age, :sex, :user_image_id, :password, :mail_address, :introduction)
+	end
+	def visit_params
+		params.require(:user).permit(:country_id)
+	end
 end
+
