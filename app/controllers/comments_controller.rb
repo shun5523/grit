8759c76d078bd0a7cfb62a947f657c4e8d992ef8
shun5523 <z31sabyss@yahoo.co.jp>
2019-11-11 09:32:31 +0000

    created comment's forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,19 @@
 class CommentsController < ApplicationController
 	def create
+		problem = Problem.find(params[:problem_id])
+		@comment = current_user.comments.new(comment_params)
+		@comment.problem_id = problem.id
+		@comment.save
+		redirect_to problem_path(problem.id)
 	end
 
 	def destroy
 	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:user_id, :problem_id, :comment)
+	end
+
+
 end
